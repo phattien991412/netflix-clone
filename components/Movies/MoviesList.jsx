ea6c7dc9c05adda7143d2927edadf28febf298a3
Phat Tien <phattien991412@gmail.com>
2023-08-08 13:21:55 +0700

    update message, list movie

--- components/Movies/MoviesList.jsx
+++ components/Movies/MoviesList.jsx
@@ -11,43 +11,12 @@ const MoviesList = ({ data, title, setLoading }) => {
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
         </p>
-        <Swiper
-          style={{
-            "--swiper-navigation-color": "#fff",
-            "--swiper-pagination-color": "#fff"
-          }}
-          spaceBetween={10}
-          slidesPerView={4}
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-          breakpoints={{
-            280: {
-              slidesPerView: 2
-            },
-            420: {
-              slidesPerView: 2
-            },
-            576: {
-              slidesPerView: 2
-            },
-            640: {
-              slidesPerView: 3
-            },
-            768: {
-              slidesPerView: 3
-            },
-            1024: {
-              slidesPerView: 4
-            }
-          }}
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4"
         >
           {data.map((movie) => (
-            <SwiperSlide key={movie.id}>
               <MovieItem data={movie} setLoading={setLoading} />
-            </SwiperSlide>
           ))}
-        </Swiper>
+        </div>
       </div>
     </div>
   );
